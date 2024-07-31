92f579b1d645bc77b872d154c1e8b5ca5ecc2b7c
Daria Merlier <daria.rodier@gmail.com>
2024-07-31 12:28:49 -0400

    dark mode update

--- src/rars/venus/editors/jeditsyntax/SyntaxUtilities.java
+++ src/rars/venus/editors/jeditsyntax/SyntaxUtilities.java
@@ -135,6 +135,7 @@ public class SyntaxUtilities {
         styles[Token.LITERAL1] = new SyntaxStyle(new Color(0x00CC66), false, false);
         styles[Token.LITERAL2] = new SyntaxStyle(new Color(0x00CC66), false, false);
         styles[Token.LABEL] = new SyntaxStyle(new Color(0xFFCC66), true, false);
+        styles[Token.SYMBOL] = new SyntaxStyle(new Color(0x00AAAAA), false, false);
         styles[Token.OPERATOR] = new SyntaxStyle(Color.black, false, true);
         styles[Token.INVALID] = new SyntaxStyle(Color.red, false, false);
         styles[Token.MACRO_ARG] = new SyntaxStyle(new Color(150, 150, 0), false, false);
