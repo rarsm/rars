e7e74fa318c30ff6348c398c548dda79711a6cb8
Jean Privat <jean@pryen.org>
2026-07-30 20:56:41 -0400

    start_at_main_ret: call Exit2 (with a val) instead of Exit

--- src/rars/assembler/Assembler.java
+++ src/rars/assembler/Assembler.java
@@ -286,14 +286,14 @@ public class Assembler {
             } // end of assembler second pass.
 
         }
-        // if the setting START AT MAIN is checked, we simulate a fake text segment
+        // if the setting START AT MAIN is checked, we inject a synthetic return pad
         // and allow the user to put "ret" in main to emulate a compiled code.
         if (Globals.getSettings().getBooleanSetting(Settings.Bool.START_AT_MAIN)) {
             int endOfTextOffset = textAddress.get();
             Memory.addressEndOfTextSegment = endOfTextOffset;
-            ProgramStatement fakeInstr1 = new ProgramStatement(0xa00893, endOfTextOffset );
+            ProgramStatement fakeInstr1 = new ProgramStatement(0x05d00893, endOfTextOffset ); // li a7, 93 ; exit2
             machineList.add(fakeInstr1);
-            ProgramStatement fakeInstr2 = new ProgramStatement(0x73, endOfTextOffset + 4);
+            ProgramStatement fakeInstr2 = new ProgramStatement(0x73, endOfTextOffset + 4); // ecall
             machineList.add(fakeInstr2);
         }
 
